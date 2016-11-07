ae799fecadd77cb06e6cd1c90b29d44f3df4ee9f
kfCy <yangkaifeng1985@126.com>
2016-11-07 22:11:08 +0800

    添加图片验证码

--- web/biz/wy/ao/Captcha.php
+++ web/biz/wy/ao/Captcha.php
@@ -23,11 +23,34 @@ class Captcha {
 		Yii::info("picture captcha: $picStr");
 
 		// 生成图片验证码
+		var_dump(gd_info());
+		$img_handle = Imagecreate(80, 20);  //图片大小80X20
+		$back_color = ImageColorAllocate($img_handle, 255, 255, 255); //背景颜色（白色）
+		$txt_color = ImageColorAllocate($img_handle, 0,0, 0);  //文本颜色（黑色）
+	    
+	    //加入干扰线
+		for($i=0;$i<3;$i++)
+		{
+			$line = ImageColorAllocate($img_handle,rand(0,255),rand(0,255),rand(0,255));
+			Imageline($img_handle, rand(0,15), rand(0,15), rand(100,150),rand(10,50), $line);
+		}
+	    //加入干扰象素
+		for($i=0;$i<200;$i++) 
+		{
+			$randcolor = ImageColorallocate($img_handle,rand(0,255),rand(0,255),rand(0,255));
+			Imagesetpixel($img_handle, rand()%100 , rand()%50 , $randcolor);
+		}
+
+		Imagefill($img_handle, 0, 0, $back_color);             //填充图片背景色
+		ImageString($img_handle, 28, 10, 0, $picStr, $txt_color);//水平填充一行字符串
+
+		ob_clean();   // ob_clean()清空输出缓存区    
+		header("Content-type: image/png"); //生成验证码图片    
+		Imagepng($img_handle);//显示图片
 
 		// 保存图片验证码到session中
 		$_SESSION[self::PIC_CAPTCHA] = $picStr;
 
-		var_dump($_SESSION);
 		return TRUE;
 	}
 
@@ -59,11 +82,8 @@ class Captcha {
 
 		session_start();
 
-		var_dump($picStr);
-		Yii::info("pic captcha: $picStr");
 		if (is_string($picStr) && $picStr != null && strlen($picStr) == self::PIC_CAPTCHA_LEN) {
 
-			var_dump($_SESSION);
 			if (isset($_SESSION[self::PIC_CAPTCHA])) {
 				if ($_SESSION[self::PIC_CAPTCHA] === $picStr) {
 					// 需要清除session中保存的图片验证码，前台需要重新请求生成新的验证码
@@ -92,8 +112,6 @@ class Captcha {
 	 */
 	public function verifyMsgCaptcha($msgStr) {
 
-		var_dump($msgStr);
-		Yii::info("msg captcha: $msgStr");
 		if ( is_string($msgStr) && $msgStr != null && strlen($msgStr) == self::MSG_CAPTCHA_LEN) {
 
 			if (isset($_SESSION[self::MSG_CAPTCHA])) {
